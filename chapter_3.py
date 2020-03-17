a02f2ab0dd46a392887874fe3cf5394afca9c2cd
IzabelaTymoszuk <izabelatymoszuk93@gmail.com>
2020-03-17 19:39:52 +0100

    Add chapter_3

---
+++ chapter_3.py
@@ -0,0 +1,15 @@
+import re
+
+
+def password():
+    """Returns the number of possible password combinations"""
+
+    combinations_number = 0
+
+    for number in range(372 ** 2, (809 ** 2) + 1):
+        number = str(number)
+        if not any(number[i] > number[i + 1] for i in range(5)):
+            matches = re.findall("22+|33+|44+|55+|66+|77+|88+|99+", number)
+            if len(matches) >= 2:
+                combinations_number += 1
+    return combinations_number
